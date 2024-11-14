1e6c1ed05e9068bc217055ad24fec56ed6344b1f
DorianALP <dorianalittlecookperez@gmail.com>
2024-11-14 14:48:18 -0600

    JavaFX Setup

--- src/App.java
+++ src/App.java
@@ -1,4 +1,16 @@
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane p = new Pane();
+        Scene scene = new Scene(p, 1000, 500);
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
