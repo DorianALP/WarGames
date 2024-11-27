0f88aa4587eaf7cc1285a930a9eb9fa56250698f
DorianALP <dorianalittlecookperez@gmail.com>
2024-11-26 21:08:11 -0600

    UML Recon

--- src/WarSimulation.java
+++ src/WarSimulation.java
@@ -1,3 +1,12 @@
-class WarSimulation extends Game {
-    
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+class WarSimulation {
+    void startGame(Stage stage) {
+        Pane p = new Pane();
+        Scene s = new Scene(p, 1000, 500);
+        stage.setScene(s);
+        stage.setFullScreen(true);
+    }
 }
